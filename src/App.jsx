687f9fcb1631b099016749ee5d1830f2bf668147
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-11-15 06:55:45 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './Pages/Home'
+import "./assets/scss/main.scss"
+
+function App() {
+
+
+  return (
+    <>
+      <Home/> 
+    </>
+  ) 
+}
+
+export default App
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./index.scss"
+
+const Home = () => {
+  return (
+    <div className='red'>index</div>
+  )
+}
+
+export default Home
